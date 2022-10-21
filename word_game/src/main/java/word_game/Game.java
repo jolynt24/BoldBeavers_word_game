50d9d7c7ca3add4ededc14bf6ecbcf491d5344a8
test <test@test.com>
2022-10-21 11:45:49 +0100

    delegated tasks

--- word_game/src/main/java/word_game/Game.java
+++ word_game/src/main/java/word_game/Game.java
@@ -9,38 +9,47 @@ public class Game {
 	public int goalX;
 	public int goalY;
 	
+	// FS
 	public void setBounds() {
 		System.out.println("CODE ME!");
 	}	
 	
+	// FS
 	public void checkBounds() {
 		System.out.println("CODE ME!");
 	}	
 	
+	// MM
 	public void move() {
 		System.out.println("CODE ME!");
 	}
 
+	// JT
 	public void checkGoal() {
 		System.out.println("CODE ME!");
 	}
 	
+	// JT
 	public void setGoal() {
 		System.out.println("CODE ME!");
 	}
 	
+	// MM
 	public void updatePos() {
 		System.out.println("CODE ME!");
 	}
 	
+	// CR
 	public void showResult() {
 		System.out.println("CODE ME!");
 	}
 	
+	// FR
 	public void quit() {
 		System.out.println("CODE ME!");
 	}
 
+	// JT
 	public void start() {
 		System.out.println("CODE ME!");
 	}
--- word_game/src/main/java/word_game/UserInput.java
+++ word_game/src/main/java/word_game/UserInput.java
@@ -2,6 +2,7 @@ package word_game;
 
 public class UserInput {
 
+	// CR
 	public void getInput() {
 		System.out.println("CODE ME!");
 	}	
